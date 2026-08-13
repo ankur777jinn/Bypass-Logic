2e0e200f4949ff4bb5cbd8c5d3d865845ba7ace5
Ankur Dahiya <ankurdahiya1102006@gmail.com>
2026-08-14 03:15:23 +0530

    Pin trl to 0.9.6 to fix SFTTrainer partial function bug

--- run_full_pipeline.sh
+++ run_full_pipeline.sh
@@ -56,7 +56,7 @@ pip install -q torch --index-url https://download.pytorch.org/whl/cu121
 pip install -q \
     transformers \
     peft \
-    trl \
+    trl==0.9.6 \
     datasets \
     accelerate \
     bitsandbytes \
